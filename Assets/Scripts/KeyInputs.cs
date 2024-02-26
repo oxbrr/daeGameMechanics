f4f8500e9c3fb783e5d58d0f120c3a777917dbc9
oxbrr <manuelmarroquin4404@gmail.com>
2024-02-26 18:09:56 -0500

    actually started working on the gamemechanics. Added Jeremy. He's cool.

--- Assets/Scripts/KeyInputs.cs
+++ Assets/Scripts/KeyInputs.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class KeyInputs : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+    public float moveSpeed = 2f; // Adjust this value to set the speed of movement
+    public float jumpPower = 6f;
+
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            Jump();
+            myRigidbody.velocity = Vector2.up * jumpPower;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            myRigidbody.velocity = Vector2.right * moveSpeed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            myRigidbody.velocity = Vector2.left * moveSpeed;
         }
     }
-    void Jump()
-    {
-
-    }
-
 }
